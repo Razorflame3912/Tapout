82c6708d7bb91a247288389be862a0a7a8aedd1a
Razorflame3912 <melsharawy@stuy.edu>
2018-06-16 10:22:01 -0400

    baseline stuff

--- tapout/__init__.py
+++ tapout/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, session, request, flash
 import os, json
-from utils import db, search
 from functools import wraps
 
 app = Flask(__name__)
@@ -10,19 +9,12 @@ DIR = os.path.dirname(__file__) or '.'
 app.secret_key = 'legit_secret_key'
 
 
-#authentication wrapper
-def require_login(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        if 'uid' not in session:
-            flash("Please log in")
-            return redirect(url_for("root"))
-        else:
-            return f(*args, **kwargs)
-    return inner
-
 @app.route("/")
 def root():
-  if "uid" in session:
-    return redirect(url_for("home"))
-  return render_template("login.html")
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
